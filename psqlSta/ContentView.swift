57d11e653c729ff0fc97de57e3eae319fc3ec75f
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-05-17 18:44:46 +0900

    Initial commit

---
+++ psqlSta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  psqlSta
+//
+//  Created by 茂木史明 on 2026/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ psqlSta/psqlStaApp.swift
@@ -0,0 +1,17 @@
+//
+//  psqlStaApp.swift
+//  psqlSta
+//
+//  Created by 茂木史明 on 2026/05/17.
+//
+
+import SwiftUI
+
+@main
+struct psqlStaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
